8d3bd866e59c1033b8c57442d9032b8afd4b6d42
francisedulab <francis@edulab.in>
2023-01-10 16:38:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'ng_http_demo';
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  user: any;
+  imagesrc:any;
+  constructor(private http: HttpClient) {
+    this.http.get('https://randomuser.me/api').subscribe((res: any) => {
+      this.user = res.results[0];
+      this.imagesrc=this.user?.picture?.large;
+      console.log(this.user);
+    })
+  }
 }
